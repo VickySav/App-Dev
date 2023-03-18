57c4214ebd289e1b47d99ad4ca22c1114b866e6d
VickySav <127477974+VickySav@users.noreply.github.com>
2023-03-19 00:12:39 +0700

    Add files via upload

---
+++ THA_W4_TJOK_SAVITRI/THA_W4_TJOK_SAVITRI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace THA_W4_TJOK_SAVITRI
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 instance;
+        public Form1 firstForm;
+
+        public string warna;
+        bool klik = false;
+      
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        public void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBoxName.Text) || String.IsNullOrEmpty(textBoxFavArtist.Text) || (String.IsNullOrEmpty(textBoxName.Text) && String.IsNullOrEmpty(textBoxFavArtist.Text)))
+            {
+                MessageBox.Show("PLEASE FILL ALL INPUT", "", MessageBoxButtons.OK);
+                //  } else if (klik == false)
+                //  {
+                //    MessageBox.Show("PLEASE CLICK OPEN NEXT WINDOW", "", MessageBoxButtons.OK);
+            } else
+            Form2.instance.output.Text = "Hi, my name is " + textBoxName.Text + " and my favorite artist is " + textBoxFavArtist.Text;
+        }
+
+        private void checkBoxTheContent_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxTheContent.Checked && klik == true)
+            {
+                btnSubmit.Enabled = true;
+            }
+            if (!checkBoxTheContent.Checked)
+                btnSubmit.Enabled = false;
+        }
+
+        private void btnOpenNextForm_Click_1(object sender, EventArgs e)
+        {
+            klik = true;
+            Form2 form2 = new Form2();
+            form2.Show();
+
+            form2.firstForm = this;
+
+            if (checkBoxTheContent.Checked && klik == true)
+            {
+                btnSubmit.Enabled = true;
+            }
+            if (!checkBoxTheContent.Checked)
+                btnSubmit.Enabled = false;
+        }
+    }
+}
---
+++ THA_W4_TJOK_SAVITRI/THA_W4_TJOK_SAVITRI/Form2.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace THA_W4_TJOK_SAVITRI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextColor = new System.Windows.Forms.Label();
+            this.labelBgColor = new System.Windows.Forms.Label();
+            this.radioButtonBgPink = new System.Windows.Forms.RadioButton();
+            this.radioButtonBgBeige = new System.Windows.Forms.RadioButton();
+            this.radioButtonBgHoney = new System.Windows.Forms.RadioButton();
+            this.radioButtonBgRose = new System.Windows.Forms.RadioButton();
+            this.radioButtonBgPeach = new System.Windows.Forms.RadioButton();
+            this.panelBgColor = new System.Windows.Forms.Panel();
+            this.radioButtonTextBrown = new System.Windows.Forms.RadioButton();
+            this.radioButtonTextRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonTextGray = new System.Windows.Forms.RadioButton();
+            this.panelTextColor = new System.Windows.Forms.Panel();
+            this.checkBoxAgreeTerms = new System.Windows.Forms.CheckBox();
+            this.checkBoxTheChoice = new System.Windows.Forms.CheckBox();
+            this.btnMagic = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.panelBgColor.SuspendLayout();
+            this.panelTextColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTextColor
+            // 
+            this.labelTextColor.AutoSize = true;
+            this.labelTextColor.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextColor.Location = new System.Drawing.Point(505, 40);
+            this.labelTextColor.Name = "labelTextColor";
+            this.labelTextColor.Size = new System.Drawing.Size(125, 22);
+            this.labelTextColor.TabIndex = 0;
+            this.labelTextColor.Text = "Pick text color";
+            // 
+            // labelBgColor
+            // 
+            this.labelBgColor.AutoSize = true;
+            this.labelBgColor.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBgColor.Location = new System.Drawing.Point(51, 40);
+            this.labelBgColor.Name = "labelBgColor";
+            this.labelBgColor.Size = new System.Drawing.Size(295, 22);
+            this.labelBgColor.TabIndex = 1;
+            this.labelBgColor.Text = "Pick your favorite background color";
+            // 
+            // radioButtonBgPink
+            // 
+            this.radioButtonBgPink.AutoSize = true;
+            this.radioButtonBgPink.Location = new System.Drawing.Point(58, 4);
+            this.radioButtonBgPink.Name = "radioButtonBgPink";
+            this.radioButtonBgPink.Size = new System.Drawing.Size(63, 24);
+            this.radioButtonBgPink.TabIndex = 2;
+            this.radioButtonBgPink.Text = "Pink";
+            this.radioButtonBgPink.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBgBeige
+            // 
+            this.radioButtonBgBeige.AutoSize = true;
+            this.radioButtonBgBeige.Location = new System.Drawing.Point(58, 25);
+            this.radioButtonBgBeige.Name = "radioButtonBgBeige";
+            this.radioButtonBgBeige.Size = new System.Drawing.Size(70, 24);
+            this.radioButtonBgBeige.TabIndex = 3;
+            this.radioButtonBgBeige.Text = "Beige";
+            this.radioButtonBgBeige.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBgHoney
+            // 
+            this.radioButtonBgHoney.AutoSize = true;
+            this.radioButtonBgHoney.Location = new System.Drawing.Point(181, 4);
+            this.radioButtonBgHoney.Name = "radioButtonBgHoney";
+            this.radioButtonBgHoney.Size = new System.Drawing.Size(78, 24);
+            this.radioButtonBgHoney.TabIndex = 4;
+            this.radioButtonBgHoney.Text = "Honey";
+            this.radioButtonBgHoney.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBgRose
+            // 
+            this.radioButtonBgRose.AutoSize = true;
+            this.radioButtonBgRose.Location = new System.Drawing.Point(181, 25);
+            this.radioButtonBgRose.Name = "radioButtonBgRose";
+            this.radioButtonBgRose.Size = new System.Drawing.Size(66, 24);
+            this.radioButtonBgRose.TabIndex = 5;
+            this.radioButtonBgRose.Text = "Rose";
+            this.radioButtonBgRose.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBgPeach
+            // 
+            this.radioButtonBgPeach.AutoSize = true;
+            this.radioButtonBgPeach.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonBgPeach.Location = new System.Drawing.Point(110, 51);
+            this.radioButtonBgPeach.Name = "radioButtonBgPeach";
+            this.radioButtonBgPeach.Size = new System.Drawing.Size(74, 24);
+            this.radioButtonBgPeach.TabIndex = 6;
+            this.radioButtonBgPeach.Text = "Peach";
+            this.radioButtonBgPeach.UseVisualStyleBackColor = false;
+            // 
+            // panelBgColor
+            // 
+            this.panelBgColor.Controls.Add(this.radioButtonBgPeach);
+            this.panelBgColor.Controls.Add(this.radioButtonBgRose);
+            this.panelBgColor.Controls.Add(this.radioButtonBgHoney);
+            this.panelBgColor.Controls.Add(this.radioButtonBgBeige);
+            this.panelBgColor.Controls.Add(this.radioButtonBgPink);
+            this.panelBgColor.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelBgColor.Location = new System.Drawing.Point(44, 60);
+            this.panelBgColor.Name = "panelBgColor";
+            this.panelBgColor.Size = new System.Drawing.Size(310, 81);
+            this.panelBgColor.TabIndex = 7;
+            // 
+            // radioButtonTextBrown
+            // 
+            this.radioButtonTextBrown.AutoSize = true;
+            this.radioButtonTextBrown.Location = new System.Drawing.Point(25, 10);
+            this.radioButtonTextBrown.Name = "radioButtonTextBrown";
+            this.radioButtonTextBrown.Size = new System.Drawing.Size(79, 24);
+            this.radioButtonTextBrown.TabIndex = 8;
+            this.radioButtonTextBrown.TabStop = true;
+            this.radioButtonTextBrown.Text = "Brown";
+            this.radioButtonTextBrown.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTextRed
+            // 
+            this.radioButtonTextRed.AutoSize = true;
+            this.radioButtonTextRed.Location = new System.Drawing.Point(136, 12);
+            this.radioButtonTextRed.Name = "radioButtonTextRed";
+            this.radioButtonTextRed.Size = new System.Drawing.Size(59, 24);
+            this.radioButtonTextRed.TabIndex = 9;
+            this.radioButtonTextRed.TabStop = true;
+            this.radioButtonTextRed.Text = "Red";
+            this.radioButtonTextRed.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTextGray
+            // 
+            this.radioButtonTextGray.AutoSize = true;
+            this.radioButtonTextGray.Location = new System.Drawing.Point(83, 34);
+            this.radioButtonTextGray.Name = "radioButtonTextGray";
+            this.radioButtonTextGray.Size = new System.Drawing.Size(66, 24);
+            this.radioButtonTextGray.TabIndex = 10;
+            this.radioButtonTextGray.TabStop = true;
+            this.radioButtonTextGray.Text = "Gray";
+            this.radioButtonTextGray.UseVisualStyleBackColor = true;
+            // 
+            // panelTextColor
+            // 
+            this.panelTextColor.Controls.Add(this.radioButtonTextGray);
+            this.panelTextColor.Controls.Add(this.radioButtonTextRed);
+            this.panelTextColor.Controls.Add(this.radioButtonTextBrown);
+            this.panelTextColor.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelTextColor.Location = new System.Drawing.Point(457, 60);
+            this.panelTextColor.Name = "panelTextColor";
+            this.panelTextColor.Size = new System.Drawing.Size(220, 66);
+            this.panelTextColor.TabIndex = 11;
+            // 
+            // checkBoxAgreeTerms
+            // 
+            this.checkBoxAgreeTerms.AutoSize = true;
+            this.checkBoxAgreeTerms.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAgreeTerms.Location = new System.Drawing.Point(239, 170);
+            this.checkBoxAgreeTerms.Name = "checkBoxAgreeTerms";
+            this.checkBoxAgreeTerms.Size = new System.Drawing.Size(263, 23);
+            this.checkBoxAgreeTerms.TabIndex = 7;
+            this.checkBoxAgreeTerms.Text = "I agree to the Terms of Agreement";
+            this.checkBoxAgreeTerms.UseVisualStyleBackColor = true;
+            this.checkBoxAgreeTerms.CheckedChanged += new System.EventHandler(this.magicChecks);
+            // 
+            // checkBoxTheChoice
+            // 
+            this.checkBoxTheChoice.AutoSize = true;
+            this.checkBoxTheChoice.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxTheChoice.Location = new System.Drawing.Point(239, 196);
+            this.checkBoxTheChoice.Name = "checkBoxTheChoice";
+            this.checkBoxTheChoice.Size = new System.Drawing.Size(268, 23);
+            this.checkBoxTheChoice.TabIndex = 12;
+            this.checkBoxTheChoice.Text = "All the choice I pick above are true";
+            this.checkBoxTheChoice.UseVisualStyleBackColor = true;
+            this.checkBoxTheChoice.CheckedChanged += new System.EventHandler(this.magicChecks);
+            // 
+            // btnMagic
+            // 
+            this.btnMagic.Enabled = false;
+            this.btnMagic.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMagic.Location = new System.Drawing.Point(313, 225);
+            this.btnMagic.Name = "btnMagic";
+            this.btnMagic.Size = new System.Drawing.Size(110, 34);
+            this.btnMagic.TabIndex = 13;
+            this.btnMagic.Text = "MAGIC";
+            this.btnMagic.UseVisualStyleBackColor = true;
+            this.btnMagic.Click += new System.EventHandler(this.btnMagic_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(196, 267);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(350, 22);
+            this.labelOutput.TabIndex = 14;
+            this.labelOutput.Text = "Hi, my name is _ and my favorite artist is _";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 309);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.btnMagic);
+            this.Controls.Add(this.checkBoxTheChoice);
+            this.Controls.Add(this.checkBoxAgreeTerms);
+            this.Controls.Add(this.panelTextColor);
+            this.Controls.Add(this.panelBgColor);
+            this.Controls.Add(this.labelBgColor);
+            this.Controls.Add(this.labelTextColor);
+            this.Name = "Form2";
+            this.Text = "Second Window Form";
+            this.panelBgColor.ResumeLayout(false);
+            this.panelBgColor.PerformLayout();
+            this.panelTextColor.ResumeLayout(false);
+            this.panelTextColor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTextColor;
+        private System.Windows.Forms.Label labelBgColor;
+        private System.Windows.Forms.RadioButton radioButtonBgPink;
+        private System.Windows.Forms.RadioButton radioButtonBgBeige;
+        private System.Windows.Forms.RadioButton radioButtonBgHoney;
+        private System.Windows.Forms.RadioButton radioButtonBgRose;
+        private System.Windows.Forms.RadioButton radioButtonBgPeach;
+        private System.Windows.Forms.Panel panelBgColor;
+        private System.Windows.Forms.RadioButton radioButtonTextBrown;
+        private System.Windows.Forms.RadioButton radioButtonTextRed;
+        private System.Windows.Forms.RadioButton radioButtonTextGray;
+        private System.Windows.Forms.Panel panelTextColor;
+        private System.Windows.Forms.CheckBox checkBoxAgreeTerms;
+        private System.Windows.Forms.CheckBox checkBoxTheChoice;
+        private System.Windows.Forms.Button btnMagic;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
