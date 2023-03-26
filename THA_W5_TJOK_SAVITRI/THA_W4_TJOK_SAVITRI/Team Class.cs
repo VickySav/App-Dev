94514b7c6ca29a81152050538983a1b072392e48
VickySav <127477974+VickySav@users.noreply.github.com>
2023-03-26 18:06:58 +0700

    Add files via upload

---
+++ THA_W5_TJOK_SAVITRI/THA_W4_TJOK_SAVITRI/Team Class.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace THA_W4_TJOK_SAVITRI
+{
+    public class Team //: Player
+    {
+        private string _teamName, _teamCountry, _teamCity;
+        public List<Player> playerList;
+
+        
+        public string teamName
+        {
+            get { return _teamName; }
+            set { _teamName = value; }
+        }
+
+        public string teamCountry
+        {
+            get { return _teamCountry; }
+            set { _teamCountry = value; }
+        }
+        public string teamCity
+        {
+            get { return _teamCity; }
+            set { _teamCity = value; }
+        }
+
+        public Team(string _teamName, string _teamCountry, string _teamCity)
+        {
+            this._teamName = _teamName;
+            this._teamCountry = _teamCountry;
+            this._teamCity = _teamCity;
+            playerList = new List<Player>();
+        }
+        
+    }
+}
